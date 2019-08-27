5b64236c6bf86838c0d5adff497901b33260a915
H Brown <hebrown12@gmail.com>
2019-08-26 20:41:58 -0500

    jquery lessons

---
+++ jquery/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // do stuff
+    console.log('Hello World');
+    $('.hello').click(function(){
+        // do stuff
+        // $('.hello').addClass('goodbye'); //sorcery!!
+        $('.hello').toggleClass('goodbye');
+    })
+});
+
+// JQuery is a JavaScript framework; 
+// written to make JS easier
+// makes JS more accessible
+// '$' means jQuery
+
+
